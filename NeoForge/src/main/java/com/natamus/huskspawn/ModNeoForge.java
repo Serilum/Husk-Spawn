dfb4574253fa53702b60f3329ff23dab8ef22309
Rick South <rick@natamus.com>
2024-07-28 07:28:18 +0200

    (v3.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/huskspawn/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/huskspawn/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.huskspawn;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.huskspawn.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.huskspawn.util.Reference;
 import net.neoforged.bus.api.IEventBus;
@@ -12,6 +13,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
