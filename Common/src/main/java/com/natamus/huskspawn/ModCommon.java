c13f31692e3b5fdfb21b088af47f65643a7427f5
Rick South <rick@natamus.com>
2026-06-18 23:45:45 +0200

    Updated Husk Spawn to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/huskspawn/ModCommon.java
+++ Common/src/main/java/com/natamus/huskspawn/ModCommon.java
@@ -2,7 +2,7 @@ package com.natamus.huskspawn;
 
 import com.natamus.collective.objects.SAMObject;
 import com.natamus.huskspawn.config.ConfigHandler;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EntityTypes;
 
 public class ModCommon {
 
@@ -12,6 +12,6 @@ public class ModCommon {
 	}
 
 	private static void load() {
-		new SAMObject(EntityType.ZOMBIE, EntityType.HUSK, null, ConfigHandler.chanceZombieIsHusk, false, false, false);
+		new SAMObject(EntityTypes.ZOMBIE, EntityTypes.HUSK, null, ConfigHandler.chanceZombieIsHusk, false, false, false);
 	}
 }
